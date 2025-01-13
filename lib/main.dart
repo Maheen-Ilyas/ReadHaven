3bc8bbf5309a59a51db51768545d6952fe3f25e8
Maheen Ilyas <mahilyas05@gmail.com>
2025-01-13 22:39:33 +0300

    Onboarding done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:readhaven/views/onboarding/onboarding.dart';
 
 void main() {
@@ -11,11 +12,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Read Haven',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        fontFamily: GoogleFonts.dmSans().fontFamily,
+        scaffoldBackgroundColor: const Color(0xff14161B),
+        appBarTheme: AppBarTheme(
+          backgroundColor: const Color(0xff14161B),
+          surfaceTintColor: const Color(0xff14161B),
+        ),
       ),
+      debugShowCheckedModeBanner: false,
       home: const Onboarding(),
     );
   }
--- lib/views/onboarding/onboarding.dart
+++ lib/views/onboarding/onboarding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:readhaven/constants/app_constants.dart';
 
 class Onboarding extends StatefulWidget {
   const Onboarding({super.key});
@@ -10,6 +11,58 @@ class Onboarding extends StatefulWidget {
 class _OnboardingState extends State<Onboarding> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    double screenHeight = AppConstants.getScreenHeight(context);
+
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              SizedBox(height: screenHeight * 0.05),
+              Image.asset('assets/book.png'),
+              SizedBox(height: screenHeight * 0.06),
+              Text(
+                "Read your favorite books anywhere!",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 36.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: screenHeight * 0.005),
+              Text(
+                "Download your epub files and start reading.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color(0xff83899F),
+                  fontSize: 20.0,
+                ),
+              ),
+              SizedBox(height: screenHeight * 0.06),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xff8C31FF),
+                  minimumSize: Size(double.infinity, screenHeight * 0.062),
+                  shape: ContinuousRectangleBorder(
+                    borderRadius: BorderRadius.circular(0.0),
+                  ),
+                ),
+                child: Text(
+                  "Get Started",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
